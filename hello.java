4450cbf8d30fd7fcad07cc84ce0e9a881cc477bf
hmg <3093631482@qq.com>
2024-05-31 16:23:59 +0800

    第三次修改

--- hello.java
+++ hello.java
@@ -2,7 +2,8 @@
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello git");
-        System.out.println("bin");
-        System.out.println();
+        System.out.println("hmg");
+        //第二次修改
+        System.out.println("mmmmm");
     }
 }
