a6268f835505e7e0e7903807a5d68052bbffd92d
hmg <3093631482@qq.com>
2024-05-31 16:19:50 +0800

    第三次修改

--- hello.java
+++ hello.java
@@ -3,8 +3,7 @@ import java.sql.SQLOutput;
 public class hello {
     public static void main(String[] args) {
         System.out.println("hello git");
-        System.out.println("hmg");
-        //第二次修改
-        System.out.println("mmmmm");
+        System.out.println("bin");
+        System.out.println();
     }
 }
