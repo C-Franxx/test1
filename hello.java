c0816fe9dab5632ad83bc81db2a92bc54cac743c
hmg <3093631482@qq.com>
2024-05-31 16:08:06 +0800

    hello2

--- hello.java
+++ hello.java
@@ -4,5 +4,7 @@ public class hello {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hmg");
+        //第二次修改
+        System.out.println("mmmmm");
     }
 }
